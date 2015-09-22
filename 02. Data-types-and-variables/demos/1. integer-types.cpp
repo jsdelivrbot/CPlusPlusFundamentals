6e6dbe75ea739fbe669e67f491cf8a9a8b763f36
Doncho Minkov <DonchoMinkov@gmail.com>
2015-09-22 20:49:02 +0300

    Fixed teh demos

--- 02. Data-types-and-variables/demos/1. integer-types.cpp
+++ 02. Data-types-and-variables/demos/1. integer-types.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 
 int main() {
+    signed char millennium = 2;
     short centuries = 20;
     int years = 2000;
     unsigned int days = 730480;
     unsigned long hours = 17531520;
 
+    std::cout << (int)millennium << " millennium" << std::endl;
     std::cout << centuries << " centuries" << std::endl;
     std::cout << years << " years" << std::endl;
     std::cout << days << " days" << std::endl;
